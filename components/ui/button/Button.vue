f8bff6c404dae34ae33ae5727ca41060d20b0ec8
Hikmal Maulana Ramadhan <hikmal.ramadhan@smartfinance.co.id>
2024-01-05 23:31:02 +0700

    Add tailwind & shadcn

---
+++ components/ui/button/Button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Primitive } from 'radix-vue'
+import { buttonVariants } from '.'
+import { cn } from '@/lib/utils'
+
+interface Props  {
+  variant?: NonNullable<Parameters<typeof buttonVariants>[0]>['variant']
+  size?: NonNullable<Parameters<typeof buttonVariants>[0]>['size']
+  as?: string
+  asChild?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), $attrs.class ?? '')"
+  >
+    <slot />
+  </Primitive>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p class="text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, recusandae.</p>
+            <Card>
+                <CardHeader>
+      <CardTitle>Card Title</CardTitle>
+      <CardDescription>Card Description</CardDescription>
+    </CardHeader>
+            </Card>
+    
+
+    <Button>hi </Button>
+
+  <Input type="email" placeholder="Email" />
+    </div>
+</template>
+<script lang="ts" setup>
+</script>
