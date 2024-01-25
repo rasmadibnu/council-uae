802e137073fbc5f42d619b41198d9076b150817a
Rasmad Ibnu <rasmadibnua@gmail.com>
2024-01-25 09:29:27 +0700

    add ecosystem pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "Council UAE",
+      port: "3000",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
+      env: {
+        DATABASE_URL: "mysql://rasmadibnu:rsmdia001@localhost:3306/uae_cms",
+        JWT_SECRET_KEY: kntl0don,
+      },
+    },
+  ],
+};
